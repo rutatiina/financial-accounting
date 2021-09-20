dfb58b5d6d866b6e120f2f4fa329bbf3066e50a5
Gilbert Rutatiina <rutatiina@ruginem.org>
2021-09-20 22:49:28 +0300

    - Add the rutatiina/user package

---
+++ src/config/financial-accounting.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+
+    'version' => 2,
+
+    'accounts_receivable_code' => 120100,
+    'accounts_payables_code' => 120100,
+
+    'sales_revenue_code' => 410100,
+    'cost_of_sales_code' => 720100,
+
+    'approvable_status' => [
+        'draft'
+    ]
+
+];
