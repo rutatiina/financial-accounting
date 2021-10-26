b8249df87eb16a9852a0a8e49521741bae7a653d
Gilbert Rutatiina <rutatiina@ruginem.org>
2021-10-26 16:49:56 +0300

    update:: command to handle installation

--- src/Commands/FinancialAccountingCommand.php
+++ src/Commands/FinancialAccountingCommand.php
@@ -3,6 +3,8 @@
 namespace Rutatiina\FinancialAccounting\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class FinancialAccountingCommand extends Command
 {
@@ -11,7 +13,7 @@ class FinancialAccountingCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'rutatiina:install {--B|bundle=}';
+    protected $signature = 'rutatiina:install {--bundle=}';
 
     /**
      * The console command description.
@@ -43,10 +45,63 @@ class FinancialAccountingCommand extends Command
 
         if (!$bundle)
         {
-            $bundle = $this->anticipate('Using up / down keys, please choose bundle to install?', ['--accountant']);
+            $bundle = $this->anticipate('Using up / down keys, please choose bundle to install?', ['accountant']);
         }
 
-        $this->info('Bundle choosen is:: '.$bundle);
+        $this->info('- Bundle choosen is:: '.$bundle);
+
+        //confirm if the db config is setup
+        try
+        {
+            DB::connection('system')->getDatabaseName();
+            DB::connection('tenant')->getDatabaseName();
+        }
+        catch (\Throwable $e)
+        {
+            $this->error('Please set DB connections as required.');
+            return false;
+        }
+
+        $this->info('- Requirements for installation are all available.');
+
+        $this->info('- Running migrations.');
+
+        //php artisan db:wipe --database system && php artisan db:wipe --database tenant && php artisan migrate:fresh --seed
+        //$this->call('db:wipe --database system');
+        //$this::call('db:wipe --database tenant');
+        //$this::call('php artisan migrate:fresh --seed');
+
+        $this->info('- Running seeders.');
+
+        $this->call('db:seed', [
+            '--class' => 'Rutatiina\Item\Seeders\ItemCategoriesSeeder',
+        ]);
+
+        /*
+        $this->info('- Publishing resources.');
+
+        $this->call('vendor:publish', [
+            '--provider' => 'Spatie\Permission\PermissionServiceProvider',
+        ]);
+
+        $this->call('vendor:publish', [
+            '--tag' => 'rutatiina/item',
+            //'--force' => true
+        ]);
+        $this->call('vendor:publish', [
+            '--tag' => 'rutatiina/configs',
+            //'--force' => true
+        ]);
+        $this->call('vendor:publish', [
+            '--tag' => 'rutatiina/ui',
+            //'--force' => true
+        ]);
+
+        $this->info('- Setup the storage folder link');
+        $this->call('storage:link');
+        //*/
+
+        $this->info('- Installation complete successfuly.');
 
         return 0;
     }
