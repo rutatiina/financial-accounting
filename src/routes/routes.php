f247f9feaf7d3376c18f8dab69caf456fb3493ce
rutatiina <rutatiina@ruginem.org>
2022-06-12 11:33:34 +0300

    Add "use Illuminate\Support\Facades\Route;" to financial-accounting package route

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['web']], function() {
     Route::get('/', '\Rutatiina\FinancialAccounting\Http\Controllers\FinancialAccountingController@index');
