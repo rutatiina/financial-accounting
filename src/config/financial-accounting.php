84b9c19ad0aca9648336fb1182ac7ce60d42dada
rutatiina <rutatiina@ruginem.org>
2022-08-27 15:43:28 +0300

    bug fixes

--- src/config/financial-accounting.php
+++ src/config/financial-accounting.php
@@ -7,7 +7,7 @@ return [
     'version' => 2,
 
     'accounts_receivable_code' => 120100,
-    'accounts_payables_code' => 120100,
+    'accounts_payables_code' => 210100,
 
     'sales_revenue_code' => 410100,
     'cost_of_sales_code' => 720100,
