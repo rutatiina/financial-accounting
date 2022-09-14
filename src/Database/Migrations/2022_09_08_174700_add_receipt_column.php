e17b48e1f9cc84f8cad83b717706debc8151d0e6
rutatiina <rutatiina@ruginem.org>
2022-09-14 12:29:07 +0300

    Fix mysql migration bug

--- src/Database/Migrations/2022_09_08_174700_add_receipt_column.php
+++ src/Database/Migrations/2022_09_08_174700_add_receipt_column.php
@@ -14,7 +14,7 @@ class AddReceiptColumn extends Migration
     public function up()
     {
         Schema::connection('tenant')->table('rg_accounting_accounts', function (Blueprint $table) {
-            $table->text('receipt')->nullable()->default(0)->comment('Is the account used to receive payments')->after('payment'); //receipt : the action of receiving something or the fact of its being received.
+            $table->unsignedTinyInteger('receipt')->nullable()->default(0)->comment('Is the account used to receive payments')->after('payment'); //receipt : the action of receiving something or the fact of its being received.
         });
     }
 
