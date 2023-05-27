69630a7f11413d6ae69f7409ec55c7490eefd66c
Gilbert Rutatiina <rutatiina@gmail.com>
2023-05-28 01:11:14 +0300

    Updates & corrections

--- src/Http/Controllers/Reports/SalesByItemController.php
+++ src/Http/Controllers/Reports/SalesByItemController.php
@@ -2,17 +2,19 @@
 
 namespace Rutatiina\FinancialAccounting\Http\Controllers\Reports;
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Request as FacadesRequest;
-use Rutatiina\FinancialAccounting\Models\Account;
-use Rutatiina\Invoice\Models\InvoiceItem;
-use Rutatiina\FinancialAccounting\Models\ContactBalance;
-use Rutatiina\FinancialAccounting\Models\AccountBalance;
-use Rutatiina\Contact\Models\Contact;
 use Rutatiina\Item\Models\Item;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Rutatiina\Contact\Models\Contact;
+use Rutatiina\Sales\Models\SalesItem;
+use Rutatiina\Invoice\Models\InvoiceItem;
+use Rutatiina\FinancialAccounting\Models\Account;
+use Rutatiina\FinancialAccounting\Models\ItemBalance;
+use Rutatiina\FinancialAccounting\Models\AccountBalance;
+use Rutatiina\FinancialAccounting\Models\ContactBalance;
+use Illuminate\Support\Facades\Request as FacadesRequest;
 
 class SalesByItemController extends Controller
 {
@@ -31,51 +33,63 @@ class SalesByItemController extends Controller
 
     public function generate(Request $request)
     {
-        $openingData = $request->opening_date ?? date('Y-m-d');
-        $closingData = $request->closing_date ?? date('Y-m-d');
+        $openingDate = $request->opening_date ?? date('Y-m-d');
+        $closingDate = $request->closing_date ?? date('Y-m-d');
 
         $total_quantity = 0;
         $total_amount = 0;
 
+        $tenant = Auth::user()->tenant;
 
         #Get contacts
         $items = Item::select('id', 'name')->get();
 
+        #Get all revenue accounts
+        $revenueAccounts = Account::where('type', 'revenue')->pluck('code')->toArray();
+
         foreach ($items as &$item)
         {
-            /*
-             * -- Add the following fields to each contact --
-             * $contact->invoice_count
-             * $contact->sales
-             * $contact->sales_with_tax
-             */
-
-            #get total number of invoices per contact
-            $itemSales = InvoiceItem::whereHas("invoice")
+            #total sales
+            $salesBalances = ItemBalance::where('currency', $tenant->base_currency)
                 ->select(
-                    DB::raw('SUM(quantity) as total_quantity'),
-                    DB::raw('SUM(total) as total_amount'),
-                    DB::raw('AVG(rate) as avg_rate')
+                    DB::raw('SUM(debit) as total_debit'),
+                    DB::raw('SUM(credit) as total_credit'),
+                    DB::raw('SUM(quantity) as total_quantity')
                 )
-                //->where('type', 'item') //todo look at this
-                ->where('item_id', $item->id)
                 ->groupBy('item_id')
+                ->where('item_id', $item->id)
+                ->whereIn('financial_account_code', $revenueAccounts)
+                ->whereDate('date', '>=', $openingDate)
+                ->whereDate('date', '<=', $closingDate)
+                ->orderBy('date', 'desc')
                 ->first();
 
-            if ($itemSales)
-            {
-                $item->total_quantity = $itemSales->total_quantity;
-                $item->total_total = $itemSales->total_amount;
-                $item->avg_rate = $itemSales->avg_rate;
+            //return $salesBalances;
 
-                $total_quantity += $itemSales->total_quantity;
-                $total_amount += $itemSales->total_amount;
+            //get the AVERAGE PRICE
+            $averageRate = SalesItem::select('item_id', DB::raw('AVG(rate) as avg_rate'))
+                ->where('item_id', $item->id)
+                ->whereIn('credit_financial_account_code', $revenueAccounts)
+                ->whereHas('sale', function ($query) use ($openingDate, $closingDate) {
+                    $query->whereDate('date', '>=', $openingDate);
+                    $query->whereDate('date', '<=', $closingDate);
+                })
+                // ->groupBy('item_id')
+                ->first();
+            $item->avg_rate = $averageRate->avg_rate;
+
+            if ($salesBalances)
+            {
+                $item->total_quantity = $salesBalances->total_quantity;
+                $item->total_total = ($salesBalances->total_credit - $salesBalances->total_debit);
+
+                $total_quantity += $salesBalances->total_quantity;
+                $total_amount += ($salesBalances->total_credit - $salesBalances->total_debit);
             }
             else
             {
                 $item->total_quantity = 0;
                 $item->total_total = 0;
-                $item->avg_rate = 0;
             }
 
         }
@@ -83,8 +97,8 @@ class SalesByItemController extends Controller
         $total_avg_rate = ($total_quantity > 0) ? ($total_amount / $total_quantity) : 0;
 
         return [
-            'opening_date' => $openingData,
-            'closing_date' => $closingData,
+            'opening_date' => $openingDate,
+            'closing_date' => $closingDate,
             'items' => $items,
             'total_quantity' => $total_quantity,
             'total_amount' => $total_amount,
